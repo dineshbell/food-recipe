b42c4582796a1be1ec5a8c2a33c8321c99950148
Bellam Dinesh <dineshbellam98@gmail.com>
2023-04-14 18:20:38 +0530

    food recipe app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
+import Products from './Products'
 
-function App() {
+const App = () => {
+  const [search,setSearch] = useState('')
+  const[data,setData] = useState([])
+  const YOUR_APP_ID = "f3990469";
+  const YOUR_APP_KEY ="df216ede45e0678c181e3d5345a9ec12";
+  
+  const onSubmit =e => {
+    e.preventDefault()
+    fetch(`https://api.edamam.com/search?q=${search}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=12&calories=591-722&health=alcohol-free`).then(response=>response.json()).then(data=>setData(data.hits))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <center>
+        <h4 className='heading'>Food Recipe App</h4>
+        <form onSubmit={onSubmit}>
+          <input type = "text" value={search} onChange={(e) => setSearch(e.target.value)}/> <br/>
+          <input type="submit" value="Search" className='btn btn-primary'/>
+        </form>
+        {data.length>=1 ? <Products data={data}/>:null}
+      </center>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
